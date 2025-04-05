e0920bc2a8e404d3a1b0b6837bb7c4aa3f109cd3
Samir-ho <shakib.hossain@city.ac.uk>
2025-04-05 21:15:03 +0100

    updated insert values of each table and made some changes to the classes

--- StriversOperations/src/Models/Review.java
+++ StriversOperations/src/Models/Review.java
@@ -3,29 +3,32 @@ package Models;
 public class Review {
     private int reviewID;
     private int userID;
-    private int eventID;
+    private int bookingID;
     private int rating;
     private String comments;
+    private String response;
 
-    public Review(int reviewID, int userID, int eventID, int rating, String comments) {
+    public Review(int reviewID, int userID, int bookingID, int rating, String comments, String response) {
         this.reviewID = reviewID;
         this.userID = userID;
-        this.eventID = eventID;
+        this.bookingID = bookingID;
         this.rating = rating;
         this.comments = comments;
+        this.response = response;
     }
 
     // Getters
     public int getReviewID() { return reviewID; }
     public int getUserID() { return userID; }
-    public int getEventID() { return eventID; }
+    public int getBookingID() { return bookingID; }
     public int getRating() { return rating; }
     public String getComments() { return comments; }
+    public String getResponse() { return response; }
 
     // Setters
     public void setReviewID(int reviewID) { this.reviewID = reviewID; }
     public void setUserID(int userID) { this.userID = userID; }
-    public void setEventID(int eventID) { this.eventID = eventID; }
+    public void setEventID(int eventID) { this.bookingID = bookingID; }
     public void setRating(int rating) { this.rating = rating; }
     public void setComments(String comments) { this.comments = comments; }
 }
